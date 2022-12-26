5acbb240aa3f8a786a7e193a18b12062195f5518
seyoung5744 <seyoung574458@gmail.com>
2022-12-26 22:56:05 +0900

    initial commit

---
+++ src/main/java/com/zerobase/musinsamonitor/MusinsaMonitorApplication.java
@@ -0,0 +1,12 @@
+package com.zerobase.musinsamonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusinsaMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusinsaMonitorApplication.class, args);
+    }
+}
---
+++ src/test/java/com/zerobase/musinsamonitor/MusinsaMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.musinsamonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusinsaMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
